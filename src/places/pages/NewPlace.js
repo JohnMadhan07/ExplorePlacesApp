6cba5c88c4fcc0645f6b8c95d7c2f5c38c7cb120
Madhan <madhan12n@gmail.com>
2024-12-16 22:21:10 +0000

    Managed Form-wide state and Validity

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,80 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
+import PlaceCard from "../../shared/components/UIElements/PlaceCard";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/components/FormElements/validators";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/components/FormElements/validators";
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      };
+    default:
+      return state;
+  }
+};
 const NewPlace = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+const placeformsubmitHandler = (event)=>{
+event.preventDefault();
+console.log(formState)
+}
   return (
-    <form>
-      <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} />
-    </form>
+    <PlaceCard>
+      <form onSubmit={placeformsubmitHandler}>
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          rows={3}
+          label="description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          onInput={inputHandler}
+        />
+    
+      <button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </button>
+      </form>
+    </PlaceCard>
   );
 };
 export default NewPlace;
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,5 +1,5 @@
-import React, { useReducer } from "react";
-import PlaceCard from "../UIElements/PlaceCard";
+import React, { useReducer, useEffect } from "react";
+
 import { validate } from "./validators";
 
 const reducer = (state, action) => {
@@ -8,7 +8,7 @@ const reducer = (state, action) => {
       return {
         ...state,
         value: action.val,
-        isValid: validate(action.val,action.validators),
+        isValid: validate(action.val, action.validators),
         isTouched: true,
       };
     default:
@@ -16,51 +16,65 @@ const reducer = (state, action) => {
   }
 };
 const Input = (props) => {
-
   const [inputstate, dispatch] = useReducer(reducer, {
-    value: '',
+    value: "",
     isValid: false,
     isTouched: false,
   });
   const conditionalStyles = {
-    backgroundColor: !inputstate.isTouched || inputstate.isValid ? "white" : "#ffc7cb",
-    border: !inputstate.isTouched || inputstate.isValid ? "1px solid #4a4949" : "1px solid #a90006",
-    borderRadius:"2px"
+    backgroundColor:
+      !inputstate.isTouched || inputstate.isValid ? "white" : "#ffc7cb",
+    border:
+      !inputstate.isTouched || inputstate.isValid
+        ? "1px solid #4a4949"
+        : "1px solid #a90006",
+    borderRadius: "2px",
   };
+  const { id, onInput } = props;
+  const { value, isValid } = inputstate;
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
   const changeHandler = (event) => {
-    dispatch({ type: "CHANGE", val: event.target.value ,validators:props.validators});
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
   };
+ 
   const element =
     props.element === "input" ? (
       <input
+        id={props.id}
         type={props.type}
         onChange={changeHandler}
         value={inputstate.value}
-        style={conditionalStyles} 
+        style={conditionalStyles}
       ></input>
     ) : (
       <textarea
+        id={props.id}
         rows={props.rows || 3}
         onChange={changeHandler}
         value={inputstate.value}
       ></textarea>
     );
-    
+
   return (
-    <PlaceCard>
-      <label
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "8px",
-          justifyContent: "start",
-          width: "100%",
-        }}
-      >
-        {props.label}
-        {element}
-      </label>
-    </PlaceCard>
+    <label
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "8px",
+        justifyContent: "start",
+        width: "100%",
+      }}
+    >
+      {props.label}
+      {element}
+    </label>
   );
 };
 
