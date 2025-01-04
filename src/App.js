d0fa2ef72d39a48f88220a75d8f8c1cd561d6dbf
Madhan <madhan12n@gmail.com>
2025-01-04 13:59:02 +0000

    Create Context for Authentication

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import React from 'react';
-import{BrowserRouter as Router} from 'react-router-dom';
-import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import Users from './users/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
-import Authenticate from './users/pages/Authenticate';
+import React, { useCallback, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Authenticate from "./users/pages/Authenticate";
+import { AuthContext } from "./shared/components/context/Auth-context";
 
 function App() {
+  const [isLoggedIn, setisLoggedIn]=useState(false)
+  const login=useCallback(()=>{
+    setisLoggedIn(false)
+  },[])
+  const logout=useCallback(()=>{
+    setisLoggedIn(false)
+  },[])
   return (
-  <Router>
-    <MainNavigation></MainNavigation>
-    <Switch>
-    <Route path='/' exact><Users/></Route>
-    <Route path='/:userid/places' exact><UserPlaces></UserPlaces></Route>
-    <Route path='/places/new' exact><NewPlace></NewPlace></Route>
-    <Route path= '/places/:placeId' exact><UpdatePlace/></Route>
-    <Route path="/auth" exact><Authenticate></Authenticate></Route>
-    <Redirect to='/'></Redirect>
-    </Switch>
-  </Router>
-  )
+    <AuthContext.Provider value={{isLoggedIn:isLoggedIn, login:login, logout:logout}}>
+      <Router>
+        <MainNavigation></MainNavigation>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userid/places" exact>
+            <UserPlaces></UserPlaces>
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace></NewPlace>
+          </Route>
+          <Route path="/places/:placeId" exact>
+            <UpdatePlace />
+          </Route>
+          <Route path="/auth" exact>
+            <Authenticate></Authenticate>
+          </Route>
+          <Redirect to="/"></Redirect>
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import Button from "@mui/material/Button";
+import { AuthContext } from "../context/Auth-context";
 
 const NavLinks = () => {
   const linkStyle = {
@@ -9,20 +10,27 @@ const NavLinks = () => {
     color: "inherit",
     marginLeft: "1rem",
   };
+  const auth = useContext(AuthContext);
   return (
     <>
       <NavLink to="/" exact style={linkStyle}>
         <Button color="inherit">Home</Button>
       </NavLink>
-      <NavLink to="u1/places/" style={linkStyle}>
-        <Button color="inherit">My Places</Button>
-      </NavLink>
-      <NavLink to="/places/new" style={linkStyle}>
-        <Button color="inherit">Add Place</Button>
-      </NavLink>
-      <NavLink to="/auth" style={linkStyle}>
-        <Button color="inherit">Authenticate</Button>
-      </NavLink>
+      {auth.isLoggedIn && (
+        <NavLink to="u1/places/" style={linkStyle}>
+          <Button color="inherit">My Places</Button>
+        </NavLink>
+      )}
+      {auth.isLoggedIn && (
+        <NavLink to="/places/new" style={linkStyle}>
+          <Button color="inherit">Add Place</Button>
+        </NavLink>
+      )}
+      {!auth.isLoggedIn && (
+        <NavLink to="/auth" style={linkStyle}>
+          <Button color="inherit">Authenticate</Button>
+        </NavLink>
+      )}
     </>
   );
 };
